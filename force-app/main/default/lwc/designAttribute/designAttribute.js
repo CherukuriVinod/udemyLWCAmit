03ca3961a1277092fe1d4e690c327acbda4b8669
cherukurivinod <vinod.cherukuris@gmail.com>
2021-09-28 02:22:00 -0400

    Changes

---
+++ force-app/main/default/lwc/designAttribute/designAttribute.js
@@ -0,0 +1,9 @@
+import { LightningElement,api,track } from 'lwc';
+
+export default class DesignAttribute extends LightningElement {
+    @api height;
+    @api width;
+    @api message;
+    @track newMessage;
+
+}
