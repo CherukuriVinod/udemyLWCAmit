862aea43505d0e3d564afeef369e774b2848566b
cherukurivinod <vinod.cherukuris@gmail.com>
2021-10-01 00:27:54 -0400

    Beer Orders

---
+++ force-app/main/default/lwc/progressIndicatorComponent/progressIndicatorComponent.js
@@ -0,0 +1,11 @@
+import { LightningElement,api } from 'lwc';
+
+export default class ProgressIndicatorComponent extends LightningElement {
+    @api currentStep = 'Contact';
+
+    @api stepValues = [
+        {label : 'Account', value: 'Account'},
+        {label : 'Contact', value: 'Contact'},
+        {label : 'Opportunity', value: 'Opportunity'},
+    ]
+}
